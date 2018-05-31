4a8d369214a1d0b170fc3610ca8e0ba525431482
Kirameki Hanako <hanako@example.com>
2018-05-31 06:11:33 +0000

    modified

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,7 +45,8 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'status' => 'required', ]);
+            'status' => 'required|max:10',
+            'content' => 'required|max:191, ']);
         $task = new Task;
         $task->status = $request->status;
 	$task->content = $request->content;
@@ -92,7 +93,8 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'status' => 'required',]);
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',]);
             
      $task = Task::find($id);
      $task->status = $request->status;
