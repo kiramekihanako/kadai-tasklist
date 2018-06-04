dc7ec47f3e7079c1e1e4361942812fb5591ada82
Kirameki Hanako <hanako@example.com>
2018-06-04 04:54:50 +0000

    bootstrap design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,32 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <title>TaskBoard</title>
+        
+        <!-- Bootstrap CSS-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        
     </head>
     
     <body>
+        @include('commons.navbar')
         
-        @include('commons.error_tasks')
+         <div class="container">
+           
+                @include('commons.error_tasks')
         
-        @yield('content')
+        
+                @yield('content')
+         </div>
+                
+        
     </body>
 </html>
